5daf733ace3ca4295369f6da8b462435521ca3f1
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-16 18:30:49 +0530

    Added helper to calculate average

---
+++ DemoApplication/Helpers/ReportHelper.cs
@@ -0,0 +1,29 @@
+﻿using DemoApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApplication.Helpers
+{
+    public class ReportHelper
+    {
+        public static List<StudentAverageViewModel> GetStudentAverages()
+        {
+            List<StudentAverageViewModel> result = new List<StudentAverageViewModel>();
+            StudentModel db = new StudentModel();
+
+            foreach (var student in db.Students.ToList())
+            {
+                result.Add(new StudentAverageViewModel
+                {
+                    FirstName = student.FirstName,
+                    LastName = student.LastName,
+                    Average = student.Marks.Sum(x => x.MarksObtained) / (decimal)student.Marks.Count()
+                });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DemoApplication/Models/StudentAverageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoApplication.Models
+{
+    public class StudentAverageViewModel
+    {
+        public StudentAverageViewModel()
+        {
+
+        }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public decimal Average { get; set; }
+    }
+}
