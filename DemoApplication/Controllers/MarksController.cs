f9e6c091198e81a583ec45e448f2effc4eec9fd0
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-16 18:10:25 +0530

    Added CRUD on marks

---
+++ DemoApplication/Controllers/MarksController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoApplication.Models;
+
+namespace DemoApplication.Controllers
+{
+    [Authorize]
+    public class MarksController : Controller
+    {
+        private StudentModel db = new StudentModel();
+
+        // GET: Marks
+        public ActionResult Index()
+        {
+            return View(db.Marks.ToList());
+        }
+
+        // GET: Marks/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mark mark = db.Marks.Find(id);
+            if (mark == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mark);
+        }
+
+        // GET: Marks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SubjectName,MarksObtained")] Mark mark)
+        {
+            if (ModelState.IsValid)
+            {
+                mark.Id = Guid.NewGuid();
+                db.Marks.Add(mark);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mark);
+        }
+
+        // GET: Marks/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mark mark = db.Marks.Find(id);
+            if (mark == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mark);
+        }
+
+        // POST: Marks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SubjectName,MarksObtained")] Mark mark)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mark).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mark);
+        }
+
+        // GET: Marks/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mark mark = db.Marks.Find(id);
+            if (mark == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mark);
+        }
+
+        // POST: Marks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Mark mark = db.Marks.Find(id);
+            db.Marks.Remove(mark);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
