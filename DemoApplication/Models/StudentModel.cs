1a39c9bfe9be64eb2a00e52982ee8c96cdde058d
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-16 18:05:46 +0530

    Updated student model

--- DemoApplication/Models/StudentModel.cs
+++ DemoApplication/Models/StudentModel.cs
@@ -6,26 +6,14 @@ namespace DemoApplication.Models
 
     public class StudentModel : DbContext
     {
-        // Your context has been configured to use a 'StudentModel' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'DemoApplication.Models.StudentModel' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'StudentModel' 
-        // connection string in the application configuration file.
         public StudentModel()
             : base("name=StudentModel")
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+        public virtual DbSet<Student> Students { get; set; }
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Mark> Marks { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
