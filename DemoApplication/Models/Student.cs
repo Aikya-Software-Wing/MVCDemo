9f8fd4ff8b04ab5442987b851b145b8771ee8d7e
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-16 17:59:11 +0530

    Added student model

---
+++ DemoApplication/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoApplication.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(1000000000, 9999999999)]
+        [Display(Name = "Phone Number")]
+        public long PhoneNumber { get; set; }
+    }
+}
